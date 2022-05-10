b1e2a6bba5f45b07e0a71e6d0f9fc66ffd5233fd
Jayan Patel <jayanhpatel@gmail.com>
2022-05-10 13:34:18 -0400

    Completed Dom 2 module

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 import './less/index.less'
+// load
+window.onload = function (evt){
+    console.log(`event ${evt.type} fired! Ready to go`)
+    const heading = document.querySelector('h1');
+    heading.textContent = 'Ready to go'
 
-// Your code goes here!
+// Copy
+window.addEventListener('copy',() => {
+    navigator.clipboard.readText()
+    .then(text =>{
+        heading.textContent += text
+    })
+})
+// click
+document.body/addEventListener('click', evt =>{
+    evt.target.classList.toggle('mirror')
+})
+// dbl click
+document.body.addEventListener('dblclick', evt =>{
+    evt.target.innerHTML =''
+})
+// key down
+window.addEventListener('keydown' , evt =>{
+    if (evt.key == 6){
+        document.body.innerHTML ='<h1>Order 66</h>'
+    }
+})
+document.body.addEventListener('mousemove', evt =>{
+    const { clientX, clientY } = evt
+})
+}
+const destinations = document.querySelectorAll('.destinations')
+for (let destination of destinations){
+    destination.addEventListener('mouseenter', () =>{
+        destination.style.fontWeight = 'bold'
+    })
+    destination.addEventListener('mouseleave', () =>{
+        destination.style.fontWeight = 'initial'
+    })
+}
